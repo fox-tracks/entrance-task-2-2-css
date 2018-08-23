4716c680a33a1510bdf36b1cf2bfd9b9bfb15a46
fox-tracks <t.andriyanova@yahoo.com>
2018-08-23 14:03:28 +0300

    filter-menu for vp < 500, filter js, mobile styles

--- src/js/own/main.js
+++ src/js/own/main.js
@@ -10,58 +10,103 @@
     hamburgerBtn.classList.toggle('hamburger-btn_action_close')
   });
 
-  // листалка scenarios
+  // листалка сценариев
 
   const bntRightScenarios = document.querySelector('.btn-slider-nav_section_scenarios.btn-slider-nav_direction_right');
   const bntLeftScenarios = document.querySelector('.btn-slider-nav_section_scenarios.btn-slider-nav_direction_left');
   const scenariosWrapper =  document.querySelector('.scenarios__wrapper');
   const scenarios = document.querySelectorAll('.scenarios__item');
-  const columnsScenarios = Math.ceil(scenarios.length / 3);
 
-  if ((window.matchMedia("(max-width: 800px)").matches && columnsScenarios > 1) ||
-      (window.matchMedia("(max-width: 1040px)").matches && columnsScenarios > 2) ||
-      (window.matchMedia("(min-width: 1041px)").matches && columnsScenarios > 3)) {
+  const possibleScenariosOffsetCount = Math.ceil(scenarios.length / 3) - 1; // максимально возможное количество смещений
+  const wrapperScenariosWidth = scenariosWrapper.getBoundingClientRect().width; // ширина врапера
+  const maxScenariosOffset = wrapperScenariosWidth * possibleScenariosOffsetCount; // максимальное смещение
+
+  // реализовано только на ширине экрана до 800px
+
+  if ((window.matchMedia("(max-width: 800px)").matches && possibleScenariosOffsetCount > 0)) {
     bntRightScenarios.classList.add('btn-slider-nav_state_active');
-  };
+  }
 
-  bntRightScenarios.addEventListener('click', (e) => {
-    let transform = getComputedStyle(scenariosWrapper).getPropertyValue('transform');
+  bntRightScenarios.addEventListener('click', () => {
+    let transformScenario = getComputedStyle(scenariosWrapper).getPropertyValue('transform'); // значение свойства трансформ врапера со страницы, строка вида 'matrix(1, 0, 0, 1, -228, 0)'
+    let translate = Math.abs(+ transformScenario.split(",")[4]); //модуль параметра транслейта
+    let coefficient;
 
-    if (transform === 'none' || transform === 'matrix(1, 0, 0, 1, 0, 0)') {
-      scenariosWrapper.style.transform = 'translateX(-100%)';
+    transformScenario === 'none' ? coefficient = 1 : coefficient = translate/ wrapperScenariosWidth + 1;
+    console.log(coefficient);
+    if ((possibleScenariosOffsetCount > 0) && ((maxScenariosOffset - translate) !== 0 || transformScenario === 'none')) {
+      scenariosWrapper.style.transform = 'translateX(-' + (wrapperScenariosWidth * coefficient) + 'px)';
       bntLeftScenarios.classList.add('btn-slider-nav_state_active');
-      columnsScenarios === 2 && bntRightScenarios.classList.remove('btn-slider-nav_state_active');
-    } else if ((transform === 'matrix(1, 0, 0, 1, -228, 0)') &&  columnsScenarios === 3) {
-      scenariosWrapper.style.transform = 'translateX(-200%)';
-      bntRightScenarios.classList.remove('btn-slider-nav_state_active');
-    } else {
-      e.preventDefault();
+      (translate + wrapperScenariosWidth === maxScenariosOffset || wrapperScenariosWidth === maxScenariosOffset) && bntRightScenarios.classList.remove('btn-slider-nav_state_active');
     }
   });
 
-  bntLeftScenarios.addEventListener('click', (e) => {
-    let transform = getComputedStyle(scenariosWrapper).getPropertyValue('transform');
+  bntLeftScenarios.addEventListener('click', () => {
+    let transformScenario = getComputedStyle(scenariosWrapper).getPropertyValue('transform'); // уже трансформ врапера со страницы, строка вида  'matrix(1, 0, 0, 1, 0, 0)'
+    let translate = Math.abs(+ transformScenario.split(",")[4]); //модуль транслейта  типа 228 или 456 ....
+    let coefficient = translate/ wrapperScenariosWidth - 1;
 
-    if(transform === 'none' || transform === 'matrix(1, 0, 0, 1, 0, 0)') {
-      e.preventDefault();
-    } else if (transform === 'matrix(1, 0, 0, 1, -228, 0)') {
-      scenariosWrapper.style.transform = 'translateX(0)';
+    if ((possibleScenariosOffsetCount > 0) && ((translate - wrapperScenariosWidth) >= 0)) {
+      scenariosWrapper.style.transform = 'translateX(-' + (wrapperScenariosWidth * coefficient) + 'px)';
       bntRightScenarios.classList.add('btn-slider-nav_state_active');
-      bntLeftScenarios.classList.remove('btn-slider-nav_state_active');
-    } else if ((transform === 'translateX(-200%)') &&  columnsScenarios === 3) {
-      scenariosWrapper.style.transform = 'translateX(-100%)';
-      bntRightScenarios.classList.add('btn-slider-nav_state_active');
+      (translate - wrapperScenariosWidth === 0) && bntLeftScenarios.classList.remove('btn-slider-nav_state_active');
     }
   });
+
+  //листалка девайсов
+
+  const bntRightDevices = document.querySelector('.btn-slider-nav_section_devices.btn-slider-nav_direction_right');
+  const bntLeftSDevices = document.querySelector('.btn-slider-nav_section_devices.btn-slider-nav_direction_left');
+  const devicesWrapper =  document.querySelector('.devices__wrapper');
+  const devices = document.querySelectorAll('.devices__item');
+  const visibleDevices = Array.from(devices).filter(item => {item.style.display = 'flex'});
+  const device = document.querySelector('.devices__item');
+
+  const wrapperDevicesWidth = devicesWrapper.getBoundingClientRect().width;
+  const deviceWidth = device.getBoundingClientRect().width;
+  const margin = (getComputedStyle(device).getPropertyValue('margin-right'));
+  const marginValue = + margin.slice(0, (margin.length - 2));
+  const devicesAtViewPort = Math.floor(wrapperDevicesWidth/(deviceWidth + marginValue));
+  const offsetDevices =  (devicesAtViewPort * (deviceWidth + marginValue));
+  const possibleDevicesOffsetCount = Math.ceil( + (visibleDevices.length / devicesAtViewPort)) - 1;
+  const maxDevicesOffset = offsetDevices * possibleDevicesOffsetCount;
+
+  bntRightDevices.addEventListener('click', () => {
+    let transformDevices = getComputedStyle(devicesWrapper).getPropertyValue('transform');
+    let translate = Math.abs(+ transformDevices.split(",")[4]);
+    let coefficient;
+
+    transformDevices === 'none' ? coefficient = 1 : coefficient = translate/ offsetDevices + 1;
+
+    devicesWrapper.style.transform = 'translateX(-' + coefficient * offsetDevices + 'px)';
+    bntLeftSDevices.classList.add('btn-slider-nav_state_active');
+    (translate + offsetDevices === maxDevicesOffset || offsetDevices === maxDevicesOffset) && bntRightDevices.classList.remove('btn-slider-nav_state_active');
+
+  });
+
+  bntLeftSDevices.addEventListener('click', () => {
+    let transformDevices = getComputedStyle(devicesWrapper).getPropertyValue('transform');
+    let translate = Math.abs(+ transformDevices.split(",")[4]);
+    let coefficient = translate/ offsetDevices - 1;
+
+    // if ((possibleScenariosOffsetCount > 0) && ((translate - wrapperScenariosWidth) >= 0)) {
+    devicesWrapper.style.transform = 'translateX(-' + coefficient * offsetDevices + 'px)';
+    bntRightDevices.classList.add('btn-slider-nav_state_active');
+    (translate - offsetDevices === 0) && bntLeftSDevices.classList.remove('btn-slider-nav_state_active');
+    // }
+  });
+
+
+
 
   // крутилка
 
-  const control = document.querySelector(".control__value"),
-    controlThumb = document.querySelector('.control__thumb'),
-    controlGrade = document.querySelector('.control__grade'),
-    pointerBox = control.getBoundingClientRect(),
-    centerPoint = window.getComputedStyle(control).transformOrigin,
-    centers = centerPoint.split(" ");
+  const control = document.querySelector(".control__value");
+  const controlThumb = document.querySelector('.control__thumb');
+  const controlGrade = document.querySelector('.control__grade');
+  const pointerBox = control.getBoundingClientRect();
+  const centerPoint = window.getComputedStyle(control).transformOrigin;
+  const centers = centerPoint.split(' ');
   let move = false;
 
 
@@ -90,7 +135,7 @@
         mouseY = pointerEvent.pageY;
       } else {
         mouseX = e.clientX,
-          mouseY = e.clientY;
+        mouseY = e.clientY;
       }
 
       const centerY = pointerBox.top + parseInt(centers[1]) - window.pageYOffset,
@@ -132,4 +177,94 @@
 
     window.addEventListener('mousemove', rotatePointer);
     window.addEventListener('touchmove', rotatePointer);
+
+
+    // открытие фильтр-меню на ширине вьюпорта < 660px и фильтрация девайсов
+
+  const filters = document.querySelectorAll('.filter-btn');
+  const btnContainer = document.querySelector('.devices__wrapper');
+  const filterMenuBtn = document.querySelector('.equipment__menu-toggle');
+  const filterMenu = document.querySelector('.filters-menu');
+
+  filters.forEach(item => {
+    item.addEventListener('click', () => {
+      filters.forEach(item => {
+        item.checked = false;
+        item.removeAttribute('checked');
+      })
+
+      item.checked = true;
+      item.setAttribute('checked', 'checked');
+      let itemId = item.id;
+
+      filterMenuBtn.textContent = item.dataset.html; // присваивание текста выбранного фильтра в кнопку открытия меню
+      filterMenu.style.display = 'none';
+
+      // фильтр девайсов
+      switch (itemId) {
+        case 'filter-btn_value_all':
+          const all = btnContainer.querySelectorAll('.filtered_all');
+          const notAll = btnContainer.querySelectorAll('.filtered:not(.filtered_all)');
+          notAll.forEach(item => item.style.display = 'none');
+          all.forEach(item => item.style.display = 'flex');
+          break;
+        case 'filter-btn_value_kitchen':
+          const kitchen = btnContainer.querySelectorAll('.filtered_kitchen');
+          const notKitchen = btnContainer.querySelectorAll('.filtered:not(.filtered_kitchen)');
+          notKitchen.forEach(item => item.style.display = 'none');
+          kitchen.forEach(item => item.style.display = 'flex');
+          break;
+        case 'filter-btn_value_room':
+          const room = btnContainer.querySelectorAll('.filtered_room');
+          const notRoom = btnContainer.querySelectorAll('.filtered:not(.filtered_room)');
+          notRoom.forEach(item => item.style.display = 'none');
+          room.forEach(item => item.style.display = 'flex');
+          break;
+        case 'filter-btn_value_bulbs':
+          const bulbs = btnContainer.querySelectorAll('.filtered_bulbs');
+          const notBulbs = btnContainer.querySelectorAll('.filtered:not(.filtered_bulbs)');
+          notBulbs.forEach(item => item.style.display = 'none');
+          bulbs.forEach(item => item.style.display = 'flex');
+          break;
+        case 'filter-btn_value_cameras':
+          const cameras = btnContainer.querySelectorAll('.filtered_cameras');
+          const notCameras = btnContainer.querySelectorAll('.filtered:not(.filtered_cameras)');
+          notCameras.forEach(item => item.style.display = 'none');
+          cameras.forEach(item => item.style.display = 'flex');
+          break;
+      }
+    })
+
+  });
+
+  // открытие фильтр меню
+
+  filterMenuBtn.addEventListener('click', ()=>{
+    filterMenu.style.display = 'flex';
+  });
+
+
+
+    // попапы
+
+  // const btnOpenPopupLight = document.querySelector('.btn_action_open-popup-light');
+  // const btnOpenPopupHeat = document.querySelector('.btn_action_open-popup-heat');
+  // const btnOpenPopupFloor = document.querySelector('.btn_action_open-popup-light');
+  //
+  // btnOpenPopupFloor.addEventListener('click', (e) => {
+  //   const btn = e.target;
+  //   const btnBox = btn.getBoundingClientRect();
+  //   const btnCenterPoint = getComputedStyle(btn).transformOrigin;
+  //   const btnCenters = centerPoint.split(' ');
+  //
+  //
+  //
+  //
+  // })
+
+
+
+
+
+
 })();
